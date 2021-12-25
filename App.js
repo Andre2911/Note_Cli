24b2c65b42e362fb3e33a2e7b67829de2176b03b
Jonathan Andre Guevara Layme <jonathan.guevara@tecsup.edu.pe>
2021-12-25 08:44:13 -0500

    terminado register cli

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import React, {useEffect, useState} from 'react';
+import { View, Text} from 'react-native';
 import { NavigationContainer  } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { Provider, useSelector,useDispatch } from 'react-redux';
+// import HomeScreen from './views/index'
 import RegisterScreen from './views/Register/register1'
 import Register2Screen from './views/Register/register2'
 import Register3Screen from './views/Register/register3'
 
 import store from './store'
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 const Stack = createNativeStackNavigator();
 const Stack_Home = createNativeStackNavigator();
 const Stack_Register = createNativeStackNavigator();
 
 
+const HomeScreens = () => {
+  return (
+    <Stack_Home.Navigator>
+      <Stack_Home.Screen name="Home2" component={RegisterScreen} options={{ headerShown: false }} />
+      {/* <Stack_Home.Screen name="Inicio" component={Timer} options={{ headerShown: false }} />
+      <Stack_Home.Screen name="settings" component={Settings} options={{ headerShown: false }} /> */}
+    </Stack_Home.Navigator>
+  )
+}
 
 const RegisterScreens = () => {
   return(
@@ -26,12 +39,42 @@ const RegisterScreens = () => {
 }
 const App = () => {
   
+  const [isAppFirstTimeOpen, setIsAppFirstTimeOpen] = useState(true);
+  const user = useSelector(state => state.auth)
+  const dispatch = useDispatch()
+
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@User')
+      // if (value === null) return setIsAppFirstTimeOpen(true);
+      setIsAppFirstTimeOpen(false);
+      if(value !== null) {
+        setIsAppFirstTimeOpen(false);
+        return value
+      }
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() =>{
+
+    getData()
+    .then((er) => {
+      if(er !== undefined) {
+        dispatch(login(er))
+      }
+    })
+  
+
+  },[])
+  if (isAppFirstTimeOpen) return <LoadingScreen />;
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName ={false ? 'Home': 'Register'}>
+      <Stack.Navigator initialRouteName ={user ? 'Home': 'Register'}>
       {
-      true ?
-        <Stack.Screen name="Home" component={RegisterScreens} options={{headerShown: false}}/>
+      user.isLoggedIn ?
+        <Stack.Screen name="Home" component={HomeScreens} options={{headerShown: false}}/>
         :
         <Stack.Screen name="Register" component={RegisterScreens} options={{ headerShown: false }} />
 }
@@ -40,7 +83,13 @@ const App = () => {
   );
 };
 
-
+const LoadingScreen = () => {
+  return(
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#000'}}>
+      <Text style={{color: 'white', fontSize: 20}}>Loading...</Text>
+    </View>
+  )
+}
 const Tienda = () => {
 
   return(
